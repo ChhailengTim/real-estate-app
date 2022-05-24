89278a5fa0262ed9e592a3254a7975ec17e25308
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-05-25 00:16:16 +0700

    fix with sizebox

--- lib/profile_screen.dart/open_house_schedule_screen.dart
+++ lib/profile_screen.dart/open_house_schedule_screen.dart
@@ -22,41 +22,63 @@ class _OpenHouseScheduleScreenState extends State<OpenHouseScheduleScreen>
               Theme.of(context).textTheme.headlineSmall!.copyWith(fontSize: 20),
         ),
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: Colors.black12,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: TabBar(
-          indicator: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          controller: tabController,
-          isScrollable: true,
-          labelPadding: const EdgeInsets.symmetric(
-            horizontal: 30,
-          ),
-          tabs: [
-            Tab(
-              child: Text(
-                "Upcomming",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 35,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TabBar(
+                indicator: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                controller: tabController,
+                isScrollable: true,
+                labelPadding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                tabs: [
+                  Tab(
+                    child: Text(
+                      "Upcomming",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(fontSize: 20),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "            Past            ",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(fontSize: 20),
+                    ),
+                  )
+                ],
               ),
             ),
-            Tab(
-              child: Text(
-                "Past",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(fontSize: 20),
-              ),
-            )
+            const SizedBox(
+              height: 200,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.open_in_browser,
+                  size: 100,
+                  color: Colors.black38,
+                ),
+                Text("Save open houses to help \n     plan your weekend."),
+              ],
+            ),
           ],
         ),
       ),
