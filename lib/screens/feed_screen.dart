8cc457838118d9215c3f80b0825d498274830711
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-05-24 14:41:46 +0700

    Update feed_screen.dart

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -13,52 +13,53 @@ class FeedScreen extends StatelessWidget {
     String? address,
     String? lister,
   }) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 200,
-                color: Colors.amber,
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.all(5.0),
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: const BoxDecoration(
-                        color: Colors.black54,
-                        // borderRadius: BorderRadius.circular(5),
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Icon(Icons.place),
-                    ),
-                  ],
-                ),
-              ),
-              Stack(
-                alignment: Alignment.bottomLeft,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              color: Colors.amber,
+              child: Stack(
+                alignment: Alignment.bottomRight,
                 children: [
                   Container(
                     margin: const EdgeInsets.all(5.0),
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(5),
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.black54,
+                      // borderRadius: BorderRadius.circular(5),
+                      shape: BoxShape.circle,
                     ),
-                    child: Text(hot ?? ""),
+                    child: const Icon(Icons.place),
                   ),
                 ],
               ),
-            ],
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 80,
-            color: Colors.white,
+            ),
+            Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(hot ?? ""),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 100,
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -92,8 +93,8 @@ class FeedScreen extends StatelessWidget {
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -127,6 +128,17 @@ class FeedScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
+              Column(
+                children: const [
+                  Icon(
+                    Icons.home,
+                    size: 50,
+                  ),
+                  Text("Welcome to your feed"),
+                  Text(
+                      "We'll recommend new homes like the ones you click and favorite.")
+                ],
+              ),
               Container(
                 alignment: Alignment.centerLeft,
                 child: Text(
